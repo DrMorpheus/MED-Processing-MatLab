6cd13c219e10029f4ea3ed45f00ed2e7e3fdf87d
DrMorpheus <matthew.whitaker@stonybrook.edu>
2015-06-10 16:30:05 -0400

    Functional Function
    Function now in working condition, optimized for speed.

--- MED-Functions/readEDF10.m
+++ MED-Functions/readEDF10.m
@@ -1,6 +1,6 @@
-% readECal - Function for reading energy calbration data stored in an EDF10  
-% file output via energy calibration in Plot85.
-% Last updated: June 9, 2015
+% readEDF10 - Function for reading energy calbration data stored in an EDF  
+% file (10 detectors) output via energy calibration in Plot85.
+% Last updated: June 10, 2015
 % Code written by Matthew L. Whitaker
 function readEDF10(filename)
 
@@ -8,10 +8,19 @@ function readEDF10(filename)
 % Initialize calibration variables.
 delimiter = ' ';
 fileID = fopen(filename,'r');
-formatSpec = '%*s%f%f%f%f%f%f%f%f%f%f%*[^\n\r]';
+formatSpec = '%f%*s%*[^\n\r]';
+startRow = [1,5,7,11,13,17,19,23,25,29,31,35,37,41,43,47,49,53,55,59];
+endRow = [3,5,9,11,15,17,21,23,27,29,33,35,39,41,45,47,51,53,57,59];
 % Read calibration data.
-ECalData = textscan(fileID, formatSpec, endRow-startRow+1, 'Delimiter', delimiter, 'MultipleDelimsAsOne', true, 'HeaderLines', startRow-1, 'ReturnOnError', false);
-% Clear temporary calibration variables
-clearvars startRow endRow formatSpec dataArray1 ans;
+dataArray = textscan(fileID, formatSpec, endRow(1)-startRow(1)+1, 'Delimiter', delimiter, 'MultipleDelimsAsOne', true, 'HeaderLines', startRow(1)-1, 'ReturnOnError', false);
+for block=2:length(startRow)
+    frewind(fileID);
+    dataArrayBlock = textscan(fileID, formatSpec, endRow(block)-startRow(block)+1, 'Delimiter', delimiter, 'MultipleDelimsAsOne', true, 'HeaderLines', startRow(block)-1, 'ReturnOnError', false);
+    dataArray{1} = [dataArray{1};dataArrayBlock{1}];
+end
 fclose(fileID);
+% Format data to ECalData specs
+D1=[dataArray{1}(1:4),dataArray{1}(5:8),dataArray{1}(9:12),dataArray{1}(13:16),dataArray{1}(17:20),dataArray{1}(21:24),dataArray{1}(25:28),dataArray{1}(29:32),dataArray{1}(33:36),dataArray{1}(37:40)];
+ECalData=num2cell(D1,[1,10]);
+% Make ECalData available in base workspace
 assignin('base', 'ECalData', ECalData);
